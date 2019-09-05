9b4234f9ed87a49c160567787bb3081972f548e3
yaroslavche <yaroslav429@gmail.com>
2019-09-05 04:34:11 +0300

    Post install script

--- src/ComposerEventHandler.php
+++ src/ComposerEventHandler.php
@@ -6,13 +6,21 @@ use Composer\Script\Event;
 
 class ComposerEventHandler
 {
-    public static function postInstall(?Event $event): void
+    /** @var string|null $projectRootDir */
+    private static $projectRootDir;
+
+    public static function postInstall(?Event $event): int
     {
         if (null === $event) {
-            return;
+            return 0;
         }
-        $vendor = 'vendor';
-        $package = 'package';
+        $io = $event->getIO();
+        while (empty($vendor)) {
+            $vendor = $io->ask('Vendor:' . PHP_EOL);
+        }
+        while (empty($package)) {
+            $package = $io->ask('Package:' . PHP_EOL);
+        }
         $packageName = sprintf('%s/%s', $vendor, $package);
         $description = 'description';
         $type = 'project';
@@ -20,11 +28,39 @@ class ComposerEventHandler
         $authorName = 'name';
         $authorEmail = 'email@email.em';
 
-        $projectRootDir = realpath(sprintf('%s%s..%s', __DIR__, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR));
-        $composerJsonFile = sprintf('%s%scomposer.json', $projectRootDir, DIRECTORY_SEPARATOR);
-        $composerJson = json_decode(file_get_contents($composerJsonFile), true);
-        // change composer.json and other files
-        array_map('unlink', array_filter((array) glob(sprintf('%s%ssrc/*', $projectRootDir, DIRECTORY_SEPARATOR))));
-        array_map('unlink', array_filter((array) glob(sprintf('%s%stests/*', $projectRootDir, DIRECTORY_SEPARATOR))));
+        self::$projectRootDir = realpath(sprintf('%s%s..%s', __DIR__, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR));
+        $composerJsonFilePath = sprintf('%s%scomposer.json', self::$projectRootDir, DIRECTORY_SEPARATOR);
+        $composerJson = json_decode(file_get_contents($composerJsonFilePath), true);
+
+        $composerJson['name'] = $packageName;
+        $composerJson['description'] = $description;
+        $composerJson['type'] = $type;
+        $composerJson['license'] = $license;
+        $composerJson['authors'][0]['name'] = $authorName;
+        $composerJson['authors'][0]['email'] = $authorEmail;
+
+        $newNamespace = sprintf('%s\\%s\\', ucfirst($vendor), ucfirst($package));
+        $composerJson['autoload']['psr-4'] = [$newNamespace => 'src/'];
+        $composerJson['autoload-dev']['psr-4'] = [sprintf('%sTests\\', $newNamespace) => 'tests/'];
+
+        unset($composerJson['scripts']['post-install-cmd']);
+
+        $composerJsonContent = json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        file_put_contents($composerJsonFilePath, $composerJsonContent);
+
+        self::clearDirectories();
+    }
+
+    private static function clearDirectories(): void
+    {
+        array_map(
+            'unlink',
+            array_filter(
+                array_merge(
+                    (array)glob(sprintf('%s%ssrc/*', self::$projectRootDir, DIRECTORY_SEPARATOR)),
+                    (array)glob(sprintf('%s%stests/*', self::$projectRootDir, DIRECTORY_SEPARATOR))
+                )
+            )
+        );
     }
 }
